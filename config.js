02f72178e1112ea67876289aec13fd1c0910da16
Clevero <Clevero@users.noreply.github.com>
2017-09-15 21:03:19 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : '',
+		wp_ws_url : 'https://meinecloud.me/turngau/wordpress/wp-appkit-api/',
+		wp_url : 'https://meinecloud.me/turngau/wordpress',
+		theme : '',
+		version : '',
+		app_title : '',
+		app_platform : 'all',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : '+|wR`vB).48f~nQwmc~q0b_GI^_g_pp*DMHydhf?Pt]Eu8hq@lKPN8@63cHPn`Z<',
+		options : {"refresh_interval":0},
+		theme_settings : [],
+		addons : []
+	};
+
+});
